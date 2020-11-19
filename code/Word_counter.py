c5a11c8e73f7b847cb3bc0e39f336ff7e81a0a47
Miklos Koren <miklos.koren@gmail.com>
2020-11-19 10:13:41 +0100

    Remove comments and testing code

--- code/Word_counter.py
+++ code/Word_counter.py
@@ -1,49 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-test_data = '''This ebook is made available at no cost and with very few
-restrictions. These restrictions apply only if (1) you make
-a change in the ebook (other than alteration for different
-display devices), or (2) you are making commercial use of
-the ebook. If either of these conditions applies, please
-check gutenberg.ca/links/licence.html before proceeding.'''
-
-
-# In[2]:
-
-
-test_data
-
-
-# In[3]:
-
-
 def words(text):
     list_of_words = []
     for word in text.split():
         list_of_words.append(word.lower())
     return list_of_words
-
-
-# (A shorter way to write this is with "list comprehension".)
-
-# In[4]:
-
-
-list_of_words = words(test_data)
-
-
-# In[5]:
-
-
-counter = {'this': 3, 'of': 4, 'conditions': 1}
-
-
-# In[26]:
-
 
 def word_count(words):
     counter = {}
@@ -57,28 +19,6 @@ def word_count(words):
         # print(counter)
     return counter
 
-
-# In[8]:
-
-
-dict_of_words = word_count(list_of_words)
-
-
-# In[9]:
-
-
-dict_of_words
-
-
-# In[10]:
-
-
-dict_of_words['this']
-
-
-# In[20]:
-
-
 def read_text(filename):
     with open(filename, 'rt', encoding='latin1') as textfile:
         print('I am inside with:')
@@ -86,53 +26,12 @@ def read_text(filename):
     print('I am now outside, file is closed.')
     return data
 
-
-# In[21]:
-
-
-old_man_and_the_sea = read_text('../data/raw/gutenberg/hemingway.txt')
-
-
-# In[22]:
-
-
-type(old_man_and_the_sea)
-
-
-# In[23]:
-
-
-list_of_words = words(old_man_and_the_sea)
-
-
-# In[24]:
-
-
-len(list_of_words)
-
-
-# In[27]:
-
-
-counter = word_count(list_of_words)
-
-
-# In[35]:
-
-
-counter['man']
-
-
 # .csv file looks like:
 # ```
 # word,count
 # sea,36
 # man,218
 # ```
-
-# In[48]:
-
-
 def write_csv(filename, counter):
     with open(filename, 'wt') as textfile:
         textfile.write('word,count\n')
@@ -141,32 +40,7 @@ def write_csv(filename, counter):
     print('Outside the "with", file is closed.')
 
 
-# In[49]:
-
-
+old_man_and_the_sea = read_text('../data/raw/gutenberg/hemingway.txt')
+list_of_words = words(old_man_and_the_sea)
+counter = word_count(list_of_words)
 write_csv('../data/derived/hemingway.csv', counter)
-
-
-# In[40]:
-
-
-str(4)
-
-
-# In[41]:
-
-
-int('4')
-
-
-# In[12]:
-
-
-help(open)
-
-
-# In[ ]:
-
-
-
-
